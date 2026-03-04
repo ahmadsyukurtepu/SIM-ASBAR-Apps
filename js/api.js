eb3bae8f681eee4aee00fb2b70632dca2fd45311
ahmadsyukurtepu <ahmadsyukurtepu@gmail.com>
2026-03-04 17:39:26 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,17 +1,15 @@
-const CONFIG = {
-    URL: "https://script.google.com/macros/s/AKfycbzgYO390noTul09XuVh5tnIlZeIgD8k6U6d5EyGFxSUoAzakdFNG-dq1H_2NpOmG__7vw/exec", // GANTI DENGAN URL BAPAK
-    USER_KEY: "user_simasbar"
-};
+// GANTI dengan URL Web App yang Anda dapatkan setelah Deploy di Google Apps Script
+const API_URL = "https://script.google.com/macros/s/AKfycbzgYO390noTul09XuVh5tnIlZeIgD8k6U6d5EyGFxSUoAzakdFNG-dq1H_2NpOmG__7vw/exec";
 
-async function callAPI(payload) {
+async function postData(data) {
     try {
-        const response = await fetch(CONFIG.URL, {
+        const response = await fetch(API_URL, {
             method: "POST",
-            body: JSON.stringify(payload)
+            body: JSON.stringify(data)
         });
         return await response.json();
     } catch (error) {
-        console.error("API Error:", error);
-        return { status: "error", message: "Koneksi terputus" };
+        console.error("Error API:", error);
+        return { status: "error", message: "Gagal terhubung ke server" };
     }
 }
